21e2578038c88351b6f82f7fa1e6ea0901b26fd2
Domodiak <kkv84271@gmail.com>
2024-10-06 16:18:16 +0300

    Add default port for backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,6 +5,6 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("Running")
 })
