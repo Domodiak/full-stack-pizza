f36501b1324646173c2f012862dcc3f20d16de50
Domodiak <kkv84271@gmail.com>
2024-10-15 14:50:18 +0300

    Add styled-components, remove sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import Order from './pages/Order'
 
 function App() {
--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,21 +1,23 @@
-import Sidebar from "../components/Sidebar"
+
 import Menu from "../components/Menu"
 import Cart from "../components/Cart"
 import Header from "../components/Header"
+import styled from "styled-components"
+
+const PageContainer = styled.div`
+    width: 100vw;
+    height: 100vh;
+`
 
 function Order() {
     return (
-        <>
-            <Sidebar />
+        <PageContainer>
+            <Header />
             <div>
-                {/* vertical */}
-                <Header />
-                <div>
-                    <Menu />
-                    <Cart />
-                </div>
+                <Menu />
+                <Cart />
             </div>
-        </>
+        </PageContainer>
     )
 }
 
